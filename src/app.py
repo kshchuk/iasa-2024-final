9abdedfaf5d7c0c2b1ebffe6f6871672296df105
Dmytro Mandziuk <dmytromk@gmail.com>
2024-02-10 18:51:38 +0200

    make button do something

--- src/app.py
+++ src/app.py
@@ -1,12 +1,27 @@
 import panel as pn
+import datetime as dt
 import ipywidgets as widgets
 import pandas as pd
 
 pn.extension("ipywidgets", sizing_mode="stretch_width")
+pn.extension("tabulator")
 
 
 def scrap_and_analyze(event):
-    pass
+    if not event:
+        return
+
+    df = pd.DataFrame({
+            "Title": ["Sample Title 1", "Sample Title 2", "Sample Title 3"],
+            "Summary": ["Sample Summary 1", "Sample Summary 2", "Sample Summary 3"],
+            "Sentiment": ["Positive", "Neutral", "Negative"],
+            "Resource": ["Reddit", "Youtube", "CNN"],
+            "Date": ["2024-02-10", "2024-02-09", "2024-02-08"],
+            "Link": ["http://example.com/link1", "http://example.com/link2", "http://example.com/link3"]
+    }, index=[1, 2, 3])
+
+    table.value = df
+    table.visible = True
 
 
 class OptionsBox:
@@ -40,6 +55,7 @@ class OptionsBox:
 
 
 options_box = OptionsBox()
+options_box.set_on_analyze_btn_pressed(scrap_and_analyze)
 user_input = options_box.options_row
 
 table = pn.widgets.Tabulator(
@@ -47,6 +63,8 @@ table = pn.widgets.Tabulator(
     layout='fit_columns',
     header_align='center',
     text_align='center',
+    selectable=False,
+    disabled=True,
     visible=False
 )
 
