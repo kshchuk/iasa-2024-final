de714dceaac54ed139a26ee7dd1377d60853f094
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2024-02-10 19:32:08 +0200

    update article structure

--- src/web_scapper/search/post.py
+++ src/web_scapper/search/post.py
@@ -1,8 +1,19 @@
+from datetime import date
+
+
+class ArticleCollection:
+    def __init__(self):
+        self.source = ''
+        self.articles = []
+
+
 class Article:
     def __init__(self, title="", content="", votes=0, comments=None):
         self.title = title
         self.content = content
         self.votes = votes
+        self.date = date.today()
+        self.link = ''
         if comments:
             self.comments = comments
         else:
